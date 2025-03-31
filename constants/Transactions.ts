51bee3f9b857b52563f764139a448455e9fe4ab2
basokant <basokanthan@gmail.com>
2025-03-30 23:04:38 -0400

    add getRandomDate function

--- constants/Transactions.ts
+++ constants/Transactions.ts
@@ -1,3 +1,8 @@
+import { faker } from "@faker-js/faker";
+import { subDays } from "date-fns/subDays";
+
+const DAYS_IN_YEAR = 365;
+
 export const depositCategories = [
   "Salary",
   "E-transfer",
@@ -39,5 +44,21 @@ export type Transaction = {
   merchant: string;
   category: Category;
 };
+
+function getRandomDate(): Date {
+  const now = new Date();
+
+  const maxDaysAgo = 2 * DAYS_IN_YEAR; // Up to 2 years ago
+  const daysAgo = faker.number.int({ min: 0, max: maxDaysAgo });
+
+  const hours = faker.number.int({ min: 0, max: 23 });
+  const minutes = faker.number.int({ min: 0, max: 59 });
+  const seconds = faker.number.int({ min: 0, max: 59 });
+
+  const date = subDays(now, daysAgo);
+  date.setHours(hours, minutes, seconds);
+
+  return date;
+}
 
 export const TRANSACTIONS: Transaction[] = [];
