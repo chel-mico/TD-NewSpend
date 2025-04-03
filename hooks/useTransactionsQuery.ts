b573d0c4a9a5efff2c6380915672d0d0edad0e97
basokant <basokanthan@gmail.com>
2025-04-03 19:57:54 -0400

    finish fetchTransactionsByCategoryAndMerchant

--- hooks/useTransactionsQuery.ts
+++ hooks/useTransactionsQuery.ts
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { isSameDay, isSameMonth, isSameWeek, isSameYear } from "date-fns";
-import { Category, TRANSACTIONS } from "@/constants/Transactions";
+import {
+  type Category,
+  type Transaction,
+  TRANSACTIONS,
+} from "@/constants/Transactions";
 
 type Frequency = "daily" | "weekly" | "monthly" | "yearly";
 
@@ -82,5 +86,51 @@ export function useTransactionsByMerchant(frequency: Frequency, period: Date) {
   return useQuery({
     queryKey: ["transactionsByCategory", frequency, period],
     queryFn: async () => fetchTransactionsByMerchant(frequency, period),
+  });
+}
+
+async function fetchTransactionsByCategoryAndMerchant(
+  frequency: Frequency,
+  period: Date,
+) {
+  const groupedByCategory = await fetchTransactionsByCategory(
+    frequency,
+    period,
+  );
+
+  // Group by merchant within each category and sort by date
+  const sortTransactionsByDate = (transactions: Transaction[]) =>
+    [...transactions].sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+    );
+
+  const groupByMerchant = (transactions: Transaction[]) =>
+    Object.fromEntries(
+      Object.entries(Object.groupBy(transactions, (tr) => tr.merchant)).map(
+        ([merchant, merchantTransactions]) => [
+          merchant,
+          sortTransactionsByDate(merchantTransactions ?? []),
+        ],
+      ),
+    );
+
+  // Group by category and merchant, and then sort by date within each group
+  return Object.entries(groupedByCategory).reduce(
+    (acc, [category, transactions]) => {
+      acc[category] = groupByMerchant(transactions);
+      return acc;
+    },
+    {} as Record<string, Record<string, Transaction[]>>,
+  );
+}
+
+export function useTransactionsByCategoryAndMerchant(
+  frequency: Frequency,
+  period: Date,
+) {
+  return useQuery({
+    queryKey: ["transactionsByCategory", frequency, period],
+    queryFn: async () =>
+      fetchTransactionsByCategoryAndMerchant(frequency, period),
   });
 }
