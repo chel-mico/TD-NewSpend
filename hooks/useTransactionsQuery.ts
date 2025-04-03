2153bfa239dde647bcbf71d7af7b64d2a8ad0763
basokant <basokanthan@gmail.com>
2025-04-03 19:31:04 -0400

    finish fetchTransactions query function

--- hooks/useTransactionsQuery.ts
+++ hooks/useTransactionsQuery.ts
@@ -1,10 +1,29 @@
-import { TRANSACTIONS } from "@/constants/Transactions";
 import { useQuery } from "@tanstack/react-query";
+import { isSameDay, isSameMonth, isSameYear } from "date-fns";
+import { TRANSACTIONS } from "@/constants/Transactions";
 
 type Frequency = "monthly" | "yearly" | "daily";
 
 async function fetchTransactions(frequency: Frequency, period: Date) {
-  return TRANSACTIONS;
+  const filtered = TRANSACTIONS.filter((tr) => {
+    const trDate = new Date(tr.date);
+
+    switch (frequency) {
+      case "daily":
+        return isSameDay(trDate, period);
+      case "monthly":
+        return isSameMonth(trDate, period);
+      case "yearly":
+        return isSameYear(trDate, period);
+      default:
+        return false;
+    }
+  });
+
+  // Sort by recency: newest first
+  return filtered.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+  );
 }
 
 export function useTransactionsQuery(frequency: Frequency, period: Date) {
